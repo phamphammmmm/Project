b2fe8b744a1703916c437d0a5cfd20121f806fa6
minhwannnn <131199158+minhwannnn@users.noreply.github.com>
2023-05-08 14:27:18 +0700

    đã sửa đổi

---
+++ Frontend/awards.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="awards.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="img">
+            <img class="style-img"
+                src="./img1/aw1.jpg"
+                alt="">
+            <div class="center">
+                <strong>Awards</strong>
+            </div>
+        </div>
+
+        <div class="row1">
+            <div class="row11"><img src="./img1/aw2.png" alt="" style="height: 600px; width: auto;"></div>
+            <div class="row12">
+                <p><STRONG>THE BEST RESTAURANT AWARDS 2022</STRONG> ceremony was a highly 
+                    anticipated event for food lovers all around the world.
+                    Finally, after much deliberation, the winner was announced, 
+                    leaving audiences excited to try out the newly crowned <STRong> BEST RESTAURANT of 2022.</STRong>
+                </p>
+            </div>
+        </div>
+        <div class="row2">
+            <div class="row21">
+                <p>The moment you step into the Michelin award-winning restaurant.  
+                    The atmosphere is elegant yet comfortable, and the staff are attentive and knowledgeable.  
+                    Dining at this Michelin award-winning restaurant is an experience like no other, 
+                    and it is no wonder that it has earned its place among the best restaurants in the world.
+                </p>
+            </div>\
+            <div class="row22">
+                <img src="./img1/aw3.jpg" alt="" style="height: 600px; width: auto;">
+            </div>   
+        </div>
+        <div class="row3">
+            <div class="row31"><img src="./img1/aw4.png" alt="" style="height: 600px; width: auto;"></div>
+            <div class="row32">
+                <p><strong>THANK YOU</strong> for considering our luxury restaurant for the award vote. 
+                    At our establishment, we strive to provide our guests with an 
+                    unforgettable experience from start to finish.  
+                    from the elegant ambiance to the expertly crafted menu items. 
+                </p>
+            </div>
+        </div>
+        <div class="row4">
+            <div class="row41">
+                <p>Dining awards are accolades given to restaurants, chefs, 
+                    and culinary professionals for their outstanding achievements 
+                    in the food industry. These awards recognize excellence in a
+                    variety of categories, such as best new restaurant, 
+                    best chef, best service, and best overall dining experience.
+                </p>
+            </div>
+            <div class="row42">
+                <img src="./img1/aw5.jpg" alt="" style="height: 600px; width: auto;">
+            </div>   
+        </div>
+    </div>
+</body>
+</html>
+
